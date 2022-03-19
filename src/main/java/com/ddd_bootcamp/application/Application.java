50d94acfb2aa2486a6768cd51e4d45913457985b
Sandeep S Jagtap <sandeepsjagtap@Sandeeps-MacBook-Pro.local>
2022-03-19 19:48:12 +0530

    Changed product name to avoid confusion with different Apple pencils

--- src/main/java/com/ddd_bootcamp/application/Application.java
+++ src/main/java/com/ddd_bootcamp/application/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Cart cart = new Cart();
-        Product product = new Product("Apple Pencil (2nd Generation)");
+        Product product = new Product("Apple Pencil");
         cart.add(product);
 
         System.out.println("Cart = " + cart);
