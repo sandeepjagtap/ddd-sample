1366d90e17ba67391ef13c9cbe2fb0558b43c93d
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2022-04-10 21:31:25 +0530

    Added sample test

--- src/main/java/com/ddd_bootcamp/domain/Product.java
+++ src/main/java/com/ddd_bootcamp/domain/Product.java
@@ -7,6 +7,10 @@ public class Product {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
---
+++ src/test/java/com/ddd_bootcamp/domain/CartTest.java
@@ -0,0 +1,23 @@
+package com.ddd_bootcamp.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void shouldAddProductToCart() {
+        Cart cart = new Cart();
+        Product product = new Product("Some test product");
+        cart.add(product);
+
+        List<Product> actual = cart.getProducts();
+
+        assertTrue(actual.size() == 1);
+        assertEquals("Some test product", actual.get(0).getName());
+
+    }
+}
